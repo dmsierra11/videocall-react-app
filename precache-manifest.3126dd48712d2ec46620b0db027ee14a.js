d2ff9a4a82bb1a8fe9e1aa15e072f29e84e18200
Daniel <dmsierra11@gmail.com>
2020-03-21 18:20:38 +0100

    Updates

---
+++ precache-manifest.3126dd48712d2ec46620b0db027ee14a.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "870e0e9ab6239c640dc1443b8a676900",
+    "url": "/videocall-react-app/index.html"
+  },
+  {
+    "revision": "a86b2c3a58269efb4388",
+    "url": "/videocall-react-app/static/css/2.1d984fbf.chunk.css"
+  },
+  {
+    "revision": "b514441cae1645922429",
+    "url": "/videocall-react-app/static/css/main.97b87d39.chunk.css"
+  },
+  {
+    "revision": "a86b2c3a58269efb4388",
+    "url": "/videocall-react-app/static/js/2.8bf0e9e4.chunk.js"
+  },
+  {
+    "revision": "b514441cae1645922429",
+    "url": "/videocall-react-app/static/js/main.aa37595f.chunk.js"
+  },
+  {
+    "revision": "35a3f315416d13504076",
+    "url": "/videocall-react-app/static/js/runtime~main.baa94e77.js"
+  },
+  {
+    "revision": "2ab67eec1a3ab14d3cc6fc3eaa072181",
+    "url": "/videocall-react-app/static/media/ag-index-background.2ab67eec.png"
+  },
+  {
+    "revision": "c9b3e104479e2fab5588080e81dfb689",
+    "url": "/videocall-react-app/static/media/ag-logo.c9b3e104.png"
+  },
+  {
+    "revision": "8504fae6a8575e0a9786f1c67bd05e69",
+    "url": "/videocall-react-app/static/media/icons.8504fae6.eot"
+  },
+  {
+    "revision": "bc005709a21d5a2af39b0744935ace95",
+    "url": "/videocall-react-app/static/media/icons.bc005709.woff"
+  },
+  {
+    "revision": "efe053d475acd04c59283fc07f718371",
+    "url": "/videocall-react-app/static/media/icons.efe053d4.ttf"
+  }
+]);
